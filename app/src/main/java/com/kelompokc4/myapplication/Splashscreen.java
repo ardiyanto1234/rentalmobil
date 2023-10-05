e1c6d1179da1e8e3d0cf2d91ca30f119f720649f
ardiyto123 <142969559+ardiyanto1234@users.noreply.github.com>
2023-10-05 22:18:01 +0700

    up

--- app/src/main/java/com/kelompokc4/myapplication/Splashscreen.java
+++ app/src/main/java/com/kelompokc4/myapplication/Splashscreen.java
@@ -1,14 +1,64 @@
-package com.kelompokc4.myapplication;
+package com.c2.arenafinder.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.airbnb.lottie.LottieAnimationView;
+
+import com.c2.arenafinder.R;
+import com.c2.arenafinder.util.UsersUtil;
+
+@SuppressLint("CustomSplashScreen")
 public class Splashscreen extends AppCompatActivity {
+
+    private UsersUtil usersUtil;
+
+    private static final int PERMISSION_REQUEST_STORAGE = 2;
+
+    private ImageView appIcon;
+
+    private LottieAnimationView loading;
+
+    private void initViews(){
+        appIcon = findViewById(R.id.splash_logo_app);
+        loading = findViewById(R.id.splash_loading);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splashscreen);
+        setContentView(R.layout.activity_splash_screen);
+        initViews();
+        usersUtil = new UsersUtil(getApplicationContext());
+
+        // remove status bar dan navigation bar
+        getWindow().getDecorView().post(() -> new Thread(() -> runOnUiThread(() -> {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+            getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.biru_gelap));
+        })).start());
+
+        new Handler().postDelayed(
+                () -> {
+
+                    if (usersUtil.isSignIn()){
+                        startActivity(new Intent(this, MainActivity.class));
+                        finish();
+                    }else {
+                        startActivity(new Intent(this, AccountActivity.class));
+                        finish();
+                    }
+                },
+                3000L
+        );
     }
+
 }
