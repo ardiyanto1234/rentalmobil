fbd01fdfac1670a5a60243409d9ef9f9f10bb111
ardiyto123 <142969559+ardiyanto1234@users.noreply.github.com>
2023-10-05 09:59:26 +0700

    up

--- app/src/main/java/com/kelompokc4/myapplication/pesanan.java
+++ app/src/main/java/com/kelompokc4/myapplication/pesanan.java
@@ -2,18 +2,18 @@ package com.kelompokc4.myapplication;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class pesanan extends Fragment {
-
+public class pesanan extends AppCompatActivity {
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_pesanan, container, false);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pesanan);
     }
 }
