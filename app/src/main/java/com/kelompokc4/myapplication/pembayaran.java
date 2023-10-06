ee5d61b9a42fc670d23e8e00e56439d93b774dbd
ardiyto123 <142969559+ardiyanto1234@users.noreply.github.com>
2023-10-07 00:41:39 +0700

    updatetgl7

--- app/src/main/java/com/kelompokc4/myapplication/pembayaran.java
+++ app/src/main/java/com/kelompokc4/myapplication/pembayaran.java
@@ -5,16 +5,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class pembayaran extends AppCompatActivity {
 
-    @SuppressLint("MissingInflatedId")
+    ImageButton btn;
+
+    @SuppressLint({"MissingInflatedId", "WrongViewCast"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pembayaran);
 
+        btn = findViewById(R.id.btnKembalibooking);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(pembayaran.this, pesanan.class));
+                Toast.makeText(pembayaran.this, "Kembali", Toast.LENGTH_SHORT).show();
 
+            }
+        });
     }
 }
+
+
+
