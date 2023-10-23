684d1e20d74d30077f12dca157a14a1b5648327b
ardiyto123 <142969559+ardiyanto1234@users.noreply.github.com>
2023-10-23 11:35:24 +0700

    update

--- app/src/main/java/com/kelompokc4/myapplication/LupaSandi.java
+++ app/src/main/java/com/kelompokc4/myapplication/LupaSandi.java
@@ -2,13 +2,30 @@ package com.kelompokc4.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LupaSandi extends AppCompatActivity {
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lupa_sandi);
+
+        btn = findViewById(R.id.buttonLanjut2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Lanjut();
+            }
+        });
+    }
+
+    public void Lanjut() {
+        Intent intent = new Intent(LupaSandi.this, otplupasandi.class);
+        startActivity(intent);
     }
 }
