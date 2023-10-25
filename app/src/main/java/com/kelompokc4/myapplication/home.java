cbc69c21f04dcff72f0ac85e9c0bde17621646ab
ardiyto123 <142969559+ardiyanto1234@users.noreply.github.com>
2023-10-25 09:59:53 +0700

    update

--- app/src/main/java/com/kelompokc4/myapplication/home.java
+++ app/src/main/java/com/kelompokc4/myapplication/home.java
@@ -1,5 +1,6 @@
 package com.kelompokc4.myapplication;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -14,11 +15,12 @@ import android.widget.TextView;
 public class home extends Fragment {
     private TextView btn;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        btn = view.findViewById(R.id.buttonBooking1);
+        btn = view.findViewById(R.id.btn7seater);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
