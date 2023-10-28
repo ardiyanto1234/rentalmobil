749b51fd81699061a56c5f0e07d3c1047edc508a
ardiyto123 <142969559+ardiyanto1234@users.noreply.github.com>
2023-10-29 01:18:35 +0700

    update

--- app/src/main/java/com/kelompokc4/myapplication/detailmobil.java
+++ app/src/main/java/com/kelompokc4/myapplication/detailmobil.java
@@ -2,13 +2,32 @@ package com.kelompokc4.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class detailmobil extends AppCompatActivity {
 
+    ImageButton gambarbtn;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detailmobil);
-    }
+      //  setContentView(R.layout.activity_detailmobil);
+//
+//        gambarbtn = findViewById(R.id.arrowback);
+//
+//        gambarbtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(detailmobil.this, daftarmobil.class));
+//                Toast.makeText(detailmobil.this, "Kembali", Toast.LENGTH_SHORT).show();
+//
+//            }
+//        });
+ }
 }
