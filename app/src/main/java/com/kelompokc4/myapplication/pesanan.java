0781742de46e4582d11c284adc22d2c307eccb3b
ardiyto123 <142969559+ardiyanto1234@users.noreply.github.com>
2023-11-04 14:45:49 +0700

    update

--- app/src/main/java/com/kelompokc4/myapplication/pesanan.java
+++ app/src/main/java/com/kelompokc4/myapplication/pesanan.java
@@ -13,6 +13,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -22,26 +23,66 @@ import com.google.android.material.button.MaterialButton;
 public class pesanan extends AppCompatActivity {
 
     ImageButton btnback;
+    EditText Nama, Alamat, Notlp, tglLahir;
 
-    @SuppressLint({"MissingInflatedId", "WrongViewCast"})
+    Button jam12, jam24, hari2, pesan ;
+    boolean isJam12Hidden = false;
+    boolean isJam24Hidden = false;
+    boolean isHari2Hidden = false;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pesanan);
 
+        jam12 = findViewById(R.id.jam12);
+        jam24 = findViewById(R.id.jam24);
+        hari2 = findViewById(R.id.hari2);
+
         btnback = findViewById(R.id.btnKembaliDetail);
 
-        btnback.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        jam12.setOnClickListener(v -> {
+                isJam24Hidden = hideButton(jam24, isJam24Hidden);
+                isHari2Hidden = hideButton(hari2, isHari2Hidden);
+        });
+
+        jam24.setOnClickListener(v -> {
+                isJam12Hidden = hideButton(jam12, isJam12Hidden);
+                isHari2Hidden = hideButton(hari2, isHari2Hidden);
+        });
+
+        hari2.setOnClickListener(v -> {
+                isJam12Hidden = hideButton(jam12, isJam12Hidden);
+                isJam24Hidden = hideButton(jam24, isJam24Hidden);
+        });
+
+
+
+
+
+
+        btnback.setOnClickListener(v->{
                 onBackPressed();
-            }
         });
     }
-        public void kembali () {
-            Intent intent = new Intent(pesanan.this, detailmobil.class);
-            startActivity(intent);
 
+    public void kembali() {
+        Intent intent = new Intent(pesanan.this, detailmobil.class);
+        startActivity(intent);
+
+    }
+    private boolean hideButton(Button button, boolean isHidden) {
+        if (isHidden) {
+            button.setEnabled(true);
+            button.setVisibility(View.VISIBLE);
+        }else{
+            button.setEnabled(false);
+            button.setVisibility(View.INVISIBLE);
+        }
+//        Toast.makeText(this, String.valueOf(isHidden), Toast.LENGTH_SHORT).show();
+
+        return !isHidden;
     }
 }
 
